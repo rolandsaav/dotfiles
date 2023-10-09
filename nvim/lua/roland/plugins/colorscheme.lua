67581e023f458845f6a2096a7cd7a56e89c4c136
Roland Saavedra <rolandsaavedra22@gmail.com>
2023-10-09 00:59:35 -0400

    reorganized file hierarchy for Lazy

--- nvim/lua/roland/plugins/colorscheme.lua
+++ nvim/lua/roland/plugins/colorscheme.lua
@@ -1,9 +1,11 @@
 return {
-  "folke/tokyonight.nvim",
+
+	"folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
   opts = {},
   config = function()
 	  vim.cmd([[colorscheme tokyonight]])
   end,
-}
+
+  }
