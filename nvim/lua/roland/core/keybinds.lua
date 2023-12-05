ee20978c50b918bc2dda4d1a8198ff2a8e3f4ce4
Roland Saavedra <rolandsaavedra22@gmail.com>
2023-12-05 13:24:35 -0500

    updated lsp keybinds and telescope binds

--- nvim/lua/roland/core/keybinds.lua
+++ nvim/lua/roland/core/keybinds.lua
@@ -3,6 +3,9 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
+vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
+
 
 vim.keymap.set('n', '<leader>e', "<cmd>NvimTreeToggle<cr>", {})
 
