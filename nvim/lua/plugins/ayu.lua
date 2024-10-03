0691d2aed00de2bc18799147dc5d1df3e0b9ee89
Roland Saavedra <rolandsaavedra22@gmail.com>
2024-10-02 23:13:53 -0400

    clean

--- nvim/lua/plugins/ayu.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "Shatur/neovim-ayu",
-    priority = 1000,
-    lazy = false,
-    config = function()
-        vim.cmd(
-            [[colorscheme ayu]])
-    end,
-}
--- nvim/lua/settings.lua
+++
@@ -1,7 +0,0 @@
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
-vim.o.relativenumber = true
-
-vim.o.hlsearch = true
-vim.o.scrolloff = 10
