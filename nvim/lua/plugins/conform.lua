928443628bdf2fa7c3af7d9976b8ff3488f8df92
Roland Saavedra <rolandsaavedra22@gmail.com>
2025-05-30 01:13:14 -0700

    add conform.lua for better formatting support

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,23 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  -- This will provide type hinting with LuaLS
+  ---@module "conform"
+  ---@type conform.setupOpts
+  opts = {
+    -- Define your formatters
+    formatters_by_ft = {
+      lua = { "stylua" },
+	  cpp = { "clang" },
+    },
+    -- Set default options
+    default_format_opts = {
+      lsp_format = "fallback",
+    },
+  },
+  init = function()
+    -- If you want the formatexpr, here is the place to set it
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
+}
