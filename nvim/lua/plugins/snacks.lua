6be15d6373eed87293cfd45c9b7dc14d0fa74436
Roland Saavedra <rolandsaavedra22@gmail.com>
2025-05-30 02:25:34 -0700

    Smooth scroll and dimming

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -5,14 +5,43 @@ return {
 	---@type snacks.Config
 	opts = {
 		picker = { enabled = true },
+		words = { enabled = true },
+		scroll = { enabled = true },
 	},
 	keys = {
-		{ "<leader>fp", function() Snacks.picker() end, desc = "Find Pickers" },
-		{ "<leader>ff", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
-		{ "-", function() Snacks.picker.explorer() end, desc = "Smart Find Files" },
-		{ "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
-		{ "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+		{ "<leader>fp", function() Snacks.picker() end,                                         desc = "Find Pickers" },
+		{ "<leader>ff", function() Snacks.picker.smart() end,                                   desc = "Smart Find Files" },
+		{ "-",          function() Snacks.picker.explorer() end,                                desc = "File Explorer" },
+		{ "<leader>/",  function() Snacks.picker.grep() end,                                    desc = "Grep" },
+		{ "<leader>fb", function() Snacks.picker.buffers() end,                                 desc = "Buffers" },
 		{ "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
-		{ "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+		{ "<leader>fg", function() Snacks.picker.git_files() end,                               desc = "Find Git Files" },
+		{
+			"<leader>td",
+			function()
+				if Snacks.dim.enabled then
+					Snacks.dim.disable()
+				Snacks.notifier.notify("Disabled dynamic dimming")
+				else
+					Snacks.dim.enable()
+					Snacks.dim()
+				Snacks.notifier.notify("Enabled dynamic dimming")
+				end
+			end,
+			desc = "Toggle dynamic dimming"
+		},
+		{
+			"<leader>ts",
+			function()
+				if Snacks.scroll.enabled then
+					Snacks.scroll.disable()
+				Snacks.notifier.notify("Disable smooth scroll")
+				else
+					Snacks.scroll.enable()
+				Snacks.notifier.notify("Enable smooth scroll")
+				end
+			end,
+			desc = "Toggle smooth scrolling"
+		},
 	}
 }
