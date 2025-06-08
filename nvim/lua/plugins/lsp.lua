f51ff26f4222aa7fae17e9e0128fd793d22ab5af
Roland Saavedra <rolandsaavedra22@gmail.com>
2025-06-07 19:34:17 -0700

    round windows by default. remove commented config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -9,9 +9,6 @@ return {
 		lazy = false,
 		opts = {},
 	},
-	-- {
-	-- 	"hrsh7th/cmp-nvim-lsp"
-	-- },
 	{
 		"folke/lazydev.nvim",
 		ft = "lua", -- only load on lua files
@@ -23,70 +20,6 @@ return {
 			},
 		},
 	},
-	-- {
-	-- 	'hrsh7th/nvim-cmp',
-	-- 	event = 'InsertEnter',
-	-- 	enabled = false,
-	-- 	config = function()
-	-- 		local cmp = require('cmp')
-	-- 		local luasnip = require("luasnip")
-	--
-	-- 		cmp.setup({
-	-- 			sources = {
-	-- 				{ name = 'nvim_lsp' },
-	-- 				{ name = 'luasnip' },
-	-- 			},
-	-- 			mapping = cmp.mapping.preset.insert({
-	-- 				-- ['<Tab>'] = cmp.mapping.complete(),
-	-- 				['<C-u>'] = cmp.mapping.scroll_docs(-4),
-	-- 				['<C-d>'] = cmp.mapping.scroll_docs(4),
-	-- 				['<C-e>'] = cmp.mapping.abort(),
-	-- 				['<CR>'] = cmp.mapping(function(fallback)
-	-- 					if cmp.visible() then
-	-- 						if luasnip.expandable() then
-	-- 							luasnip.expand()
-	-- 						else
-	-- 							cmp.confirm({
-	-- 								select = true,
-	-- 							})
-	-- 						end
-	-- 					else
-	-- 						fallback()
-	-- 					end
-	-- 				end),
-	--
-	-- 				["<Tab>"] = cmp.mapping(function(fallback)
-	-- 					if cmp.visible() then
-	-- 						cmp.select_next_item()
-	-- 					elseif luasnip.locally_jumpable(1) then
-	-- 						luasnip.jump(1)
-	-- 					else
-	-- 						fallback()
-	-- 					end
-	-- 				end, { "i", "s" }),
-	--
-	-- 				["<S-Tab>"] = cmp.mapping(function(fallback)
-	-- 					if cmp.visible() then
-	-- 						cmp.select_prev_item()
-	-- 					elseif luasnip.locally_jumpable(-1) then
-	-- 						luasnip.jump(-1)
-	-- 					else
-	-- 						fallback()
-	-- 					end
-	-- 				end, { "i", "s" }),
-	-- 			}),
-	-- 			snippet = {
-	-- 				expand = function(args)
-	-- 					require('luasnip').lsp_expand(args.body)
-	-- 				end,
-	-- 			},
-	-- 			window = {
-	-- 				completion = cmp.config.window.bordered(),
-	-- 				documentation = cmp.config.window.bordered(),
-	-- 			},
-	-- 		})
-	-- 	end
-	-- },
 	{
 		'neovim/nvim-lspconfig',
 		cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
@@ -123,6 +56,17 @@ return {
 					end, opts)
 				end,
 			})
+
+			local orig_util = vim.lsp.util.open_floating_preview
+
+			-- Make all floating windows rounded by default
+			function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+				opts = opts or {}
+				opts.border = opts.border or "rounded"
+				opts.focusable = opts.focusable or false
+				opts.max_width = opts.max_width or 80
+				return orig_util(contents, syntax, opts, ...)
+			end
 
 			require('mason-lspconfig').setup({
 				ensure_installed = {},
