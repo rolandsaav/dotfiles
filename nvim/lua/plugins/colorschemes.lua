0f04ab5690624de894afb23609a6d3dfc7eb721f
Roland Saavedra <rolandsaavedra22@gmail.com>
2025-08-04 21:35:49 -0700

    format

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,14 +1,15 @@
-return { {
-	"ellisonleao/gruvbox.nvim",
-	priority = 1000,
-	config = function()
-		require("gruvbox").setup({
-			contrast = "hard",
-			dim_inactive = false,
-			transparent_mode = true
-		})
-	end
-},
+return {
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = function()
+			require("gruvbox").setup({
+				contrast = "hard",
+				dim_inactive = false,
+				transparent_mode = true
+			})
+		end
+	},
 	{
 		"folke/tokyonight.nvim",
 		lazy = false,
